0785c162cdd92cf42b4b748e6ad5ecccdb31315c
markk87 <markk87@nycstudents.net>
2023-10-19 09:22:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,70 +3,70 @@ const books = [
     name: "To Kill a Mockingbird",
     authorFirst: "Harper",
     authorLast: "Lee",
-    publishDate: 1960,
+    publishDate: [1960],
     genre: ["mystery", "historical", "fiction"],
   },
   {
     name: "The Great Gatsby",
     authorFirst: "F. Scott",
     authorLast: "Fitzgerald",
-    publishDate: 1925,
+    publishDate: [1925],
     genre: ["mystery", "historical", "fiction"],
   },
   {
     name: "The Hobbit",
     authorFirst: "JRR",
     authorLast: "Tolkien",
-    publishDate: 1937,
+    publishDate: [1937],
     genre: ["fantasy", "fiction"],
   },
   {
     name: "Harry Potter and the Deathly Hallows",
     authorFirst: "JK",
     authorLast: "Rowling",
-    publishDate: 1997,
+    publishDate: [1997],
     genre: ["fantasy", , "fiction"],
   },
   {
     name: "Ulysses",
     authorFirst: "James",
     authorLast: "Joyce",
-    publishDate: 1918,
+    publishDate: [1918],
     genre: ["historical", "non-fiction"],
   },
   {
     name: "War and Peace",
     authorFirst: "Leo",
     authorLast: "Tolstoy",
-    publishDate: 1867,
+    publishDate: [1867],
     genre: ["historical", "fiction"],
   },
   {
     name: "Pride and Prejudice",
     authorFirst: "Jane",
     authorLast: "Austen",
-    publishDate: 1813,
+    publishDate: [1813],
     genre: ["fiction"],
   },
   {
     name: "The Catcher in the Rye",
     authorFirst: "JD",
     authorLast: "Salinger",
-    publishDate: 1951,
+    publishDate: [1951],
     genre: ["fiction"],
   },
   {
     name: "Great Expectations",
     authorFirst: "Charles",
     authorLast: "Dickens",
-    publishDate: 1861,
+    publishDate: [1861],
     genre: ["historical", "fiction"],
   },
   {
     name: "Little Women",
     authorFirst: "Lousia May",
     authorLast: "Alcott",
-    publishDate: 1868,
+    publishDate: [1868],
     genre: ["womens", "historical", "fiction"],
   },
 ];
@@ -83,6 +83,18 @@ const titles = [
   "Great Expectations",
   "Little Women",
 ];
+
+
+books.forEach((nombre) => {
+  console.log(nombre.authorFirst + " " + nombre.authorLast + " wrote " + nombre.name + " in " + nombre.publishDate)
+});
+
+
+books.forEach((molodoi) => {
+  molodoi.publishDate.forEach((publico) => console.log(publico) )});
+
+const age = books.sort((a,b) => a.publishDate-b.publishDate);
+
 
 //Array of authors and the book they wrote
 //"--- wrote --- in ---"
